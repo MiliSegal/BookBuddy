242b68382d65cf4b94b84309176432d7ff31f82e
MiliSegal <120659111+MiliSegal@users.noreply.github.com>
2024-06-23 17:54:12 +0300

    co

--- app/src/main/java/com/example/bookbuddy/MainActivity.java
+++ app/src/main/java/com/example/bookbuddy/MainActivity.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Swipe swipe;
     private ArrayList<BookModel> booksList = new ArrayList<>();
     private ImageView bookCoverImageView;
 
